b55181722ca5da41a1c795f342b8925b984bf847
v9d0g <v9d0g.4g@gmail.com>
2026-06-22 09:21:15 +0800

    Revert "style: 统一 popover-hint 为 PixelBox 同款半透明卡片"
    This reverts commit d16a45effeef36294e1e874e771dedb441c3b9a9.

--- quartz/components/pages/Content.tsx
+++ quartz/components/pages/Content.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react"
+import { PixelBox } from "@pxlkit/ui-kit"
 import { htmlToJsx } from "../../util/jsx"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "../types"
 
@@ -6,7 +7,18 @@ const Content: QuartzComponent = ({ fileData, tree }: QuartzComponentProps) => {
   const content = htmlToJsx(fileData.filePath!, tree) as ReactNode
   const classes: string[] = fileData.frontmatter?.cssclasses ?? []
   const classString = ["popover-hint", ...classes].join(" ")
-  return <article className={classString}>{content}</article>
+  return (
+    <PixelBox
+      as="article"
+      variant="outline"
+      border
+      tone="neutral"
+      padding="md"
+      className={classString}
+    >
+      {content}
+    </PixelBox>
+  )
 }
 
 export default (() => Content) satisfies QuartzComponentConstructor
